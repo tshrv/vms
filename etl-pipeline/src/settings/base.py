46dbe93e2555e087df6d2898aced0b62733ed1b3
Tushar Srivastava <tusharsrivastava162@gmail.com>
2021-06-21 00:11:32 +0530

    Revert "pipeline"
    This reverts commit cd043d1e01bcd76e3d482cf5ebb4452d8551f025.

--- etl-pipeline/src/settings/base.py
+++
@@ -1,6 +0,0 @@
-from decouple import config
-
-DB_CONTAINER_NAME = config('ETL_DB_CONTAINER_NAME')
-DB_PORT = config('ETL_DB_PORT')
-DB_CONNECTION_STRING = f'mongodb://{DB_CONTAINER_NAME}:{DB_PORT}'
-DB_NAME = config('ETL_DB_NAME')
